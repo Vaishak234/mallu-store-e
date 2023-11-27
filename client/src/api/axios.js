e69cac5400427d7fb3b71d0498fca14477ccfa77
vaishakhp <vaishakhpat2003@gmail.com>
2023-11-27 11:27:16 +0530

    3

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export default axios.create({
-    baseURL: Process.env.REACT_APP_URL,
+    baseURL: process.env.REACT_APP_URL,
     headers: { 'Content-Type': 'application/json' },
     withCredentials:true
 })
